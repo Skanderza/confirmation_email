f705ca54c2d5b804b6024044c5cd54fd4d90c2e6
skander <zahiskander@gmail.com>
2021-12-01 17:44:32 +0100

    test 3

--- verif.php
+++ verif.php
@@ -5,4 +5,15 @@ session_start();
 //Connection BDD
 $bdd =new PDO('mysql:host=localhost;dbname=confirmation_email', "root", "root");
 
+
+if(isset($_GET['id']) AND !empty($_GET['id']) AND isset($_GET['cle']) AND !empty($_GET['id'])){
+
+    $getid = $_GET['id'];
+    $getcle = $_GET['cle'];
+    
+    
+}else{
+    echo "Aucun utilisateur trouvé";
+}
+
 ?>
